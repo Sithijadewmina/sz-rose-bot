43cbc6942cfab4e2187090470f49461c21702c38
I don't know <92771213+Sithijadewmina@users.noreply.github.com>
2021-12-10 16:11:19 +0530

    Update afk.py

--- DewmiBot/modules/afk.py
+++ DewmiBot/modules/afk.py
@@ -147,9 +147,8 @@ def check_afk(update, context, user_id, fst_name, userc_id):
 
 
 __help__ = """
-@szrosebot🇱🇰
- ❍ /afk <reason> *:* mark yourself as AFK(away from keyboard).
- ❍ /brb <reason> *:* same as the afk command - but not a command.
+ ✦ /afk <reason> *:* mark yourself as AFK(away from keyboard).
+ ✦ /brb <reason> *:* same as the afk command - but not a command.
  
  When marked as AFK, any mentions will be replied to with a message to say you're not available!
  
@@ -167,7 +166,7 @@ dispatcher.add_handler(AFK_REGEX_HANDLER, AFK_GROUP)
 dispatcher.add_handler(NO_AFK_HANDLER, AFK_GROUP)
 dispatcher.add_handler(AFK_REPLY_HANDLER, AFK_REPLY_GROUP)
 
-__mod_name__ = "ᴀꜰᴋ🥡"
+__mod_name__ = "Afk⌨"
 __command_list__ = ["afk"]
 __handlers__ = [
     (AFK_HANDLER, AFK_GROUP),
